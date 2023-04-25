63caa63f8bf1b716b98ac520f01788fce0cde3ba
Duban <duvan9430@gmail.com>
2023-04-25 08:13:49 -0500

    feat:creacion maqueta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { AdminGuardService } from './services/admin-guard.service';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : HomeComponent,
+    pathMatch : 'full'
+  },
+  {
+    path : 'signup',
+    component : SignupComponent,
+    pathMatch : 'full'
+  },
+  {
+    path : 'login',
+    component : LoginComponent,
+    pathMatch : 'full'
+  },
+  {
+    path:'admin',
+    component:DashboardComponent,
+    pathMatch:'full',
+    canActivate:[AdminGuardService]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
